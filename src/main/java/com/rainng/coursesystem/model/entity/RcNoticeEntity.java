7020fdd3591e9f8d36ccdf81707d55b2acef9e30
codercql <1281371382@qq.com>
2024-05-07 22:54:18 +0800

    时间格式转换

--- src/main/java/com/rainng/coursesystem/model/entity/RcNoticeEntity.java
+++ src/main/java/com/rainng/coursesystem/model/entity/RcNoticeEntity.java
@@ -5,6 +5,8 @@ import com.baomidou.mybatisplus.annotation.TableName;
 
 import java.io.Serializable;
 import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 /**
@@ -43,10 +45,12 @@ public class RcNoticeEntity implements Serializable {
 	/**
 	 * 创建时间
 	 */
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	private Date createTime;
 	/**
 	 * 更新时间
 	 */
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	private Date updateTime;
 
 }
